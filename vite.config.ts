aeef2b0e444f31ee0df85f2036ab4d13e1a41a9e
NOHNOLIFE <760777012@qq.com>
2022-09-30 13:57:15 +0800

    dist

--- vite.config.ts
+++ vite.config.ts
@@ -29,27 +29,27 @@ export default defineConfig({
                 "description": "Manga explorer reader Viewer",
                 "icons": [
                     {
-                        "src": "/icons/icon-64.png",
+                        "src": "./icons/icon-64.png",
                         "sizes": "64x64",
                         "type": "image/png"
                     },
                     {
-                        "src": "/icons/icon-128.png",
+                        "src": "./icons/icon-128.png",
                         "sizes": "128x128",
                         "type": "image/png"
                     },
                     {
-                        "src": "/icons/icon-256.png",
+                        "src": "./icons/icon-256.png",
                         "sizes": "256x256",
                         "type": "image/png"
                     },
                     {
-                        "src": "/icons/icon-512.png",
+                        "src": "./icons/icon-512.png",
                         "sizes": "512x512",
                         "type": "image/png"
                     }
                 ],
-                "start_url": "/index.html",
+                "start_url": "./index.html",
                 "display": "fullscreen",
                 "theme_color": "#212121",
                 "background_color": "#212121"
