f75f1e85feabab52e4431aee30ea7625dc44f565
NOHNOLIFE <760777012@qq.com>
2022-10-01 16:39:39 +0800

    shortcuts and description

--- README.md
+++ README.md
@@ -3,24 +3,13 @@
 Manga explorer reader Viewer, only support folders and image files
 recommended to use Chrome with latest version
 
-[install from here](https://nohnolife.github.io/mangaview/dist/index.html)
-install button on address bar right
+# [install from here](https://nohnolife.github.io/mangaview/dist/index.html)
 
-
-## Project Setup
-
-```sh
-npm install
-```
-
-### Compile and Hot-Reload for Development
-
-```sh
-npm run dev
-```
-
-### Type-Check, Compile and Minify for Production
-
-```sh
-npm run build
-```
+#### install button on address bar right. and it will be work while offline
+![](description/desc 1.png)
+#### drag folders into the app.
+![](description/desc 2.png)
+## but there will not have history record if you refresh or reopen app
+#### switch folder
+![](description/desc 3.png)
+![](description/desc 4.png)
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import {useDropzone} from "vue3-dropzone";
-import {computed, createElementBlock, ref} from "vue";
+import {computed, watch, ref} from "vue";
 import type {Ref} from 'vue'
 import {AppFullscreen, debounce} from 'quasar'
 
@@ -17,6 +17,24 @@ let books = computed(() => {
   return list
 })
 let tab = ref('manga')
+watch(tab, () => {
+  scroll_top()
+})
+
+function scroll_top() {
+  scrollArea.value.scrollTop = 0
+  scrollArea2.value.scrollTop = 0
+}
+
+function scroll_end() {
+  scrollArea.value.scrollTop = scrollArea.value.scrollHeight
+  scrollArea2.value.scrollTop = scrollArea2.value.scrollHeight
+}
+
+function scroll_px(px: number = 2) {
+  scrollArea.value.scrollTop+=px
+}
+
 let showDragInput = ref(false)
 const {getRootProps, getInputProps} = useDropzone({multiple: true, onDrop});
 
@@ -44,25 +62,28 @@ function createUrl(file: any[]) {
 
 let showHeader = ref(true)
 let scrollVal = 0
-let scroll = (v: any) => {
-  if (v - scrollVal > 0) {
+let scroll = debounce((v: any) => {
+  if (showHeader && v - scrollVal > 60) {
     showHeader.value = false
-  } else if (v - scrollVal < 0) {
+  } else if (v - scrollVal < -60) {
     showHeader.value = true
   }
   scrollVal = v
   scrollSync()
-}
-
-let scrollSync = debounce(() => {
-  if (scrollArea.value && scrollArea2.value) {
-    (scrollArea2.value as Element).scrollTop = ((scrollArea2.value as Element).scrollHeight - (scrollArea2.value as Element).clientHeight)
-        * (scrollArea.value as Element).scrollTop / ((scrollArea.value as Element).scrollHeight - (scrollArea.value as Element).clientHeight)
-  }
 }, 200)
 
-let scrollArea = ref(null)
-let scrollArea2 = ref(null)
+let scrollSync = (revert?: boolean) => {
+  if (!revert) {
+    scrollArea2.value.scrollTop = (scrollArea2.value.scrollHeight - scrollArea2.value.clientHeight)
+        * scrollArea.value.scrollTop / (scrollArea.value.scrollHeight - scrollArea.value.clientHeight)
+  } else {
+    scrollArea.value.scrollTop = (scrollArea.value.scrollHeight - scrollArea.value.clientHeight)
+        * scrollArea2.value.scrollTop / (scrollArea2.value.scrollHeight - scrollArea2.value.clientHeight)
+  }
+}
+
+let scrollArea = ref({} as Element)
+let scrollArea2 = ref({} as Element)
 
 function refresh() {
   location.reload()
@@ -72,7 +93,8 @@ let fitWidth = ref(true);
 </script>
 
 <template>
-  <q-layout view="hHh lpR fFf" class="column full-height bg-grey-8 text-grey-1" @dragenter="dragenter"
+  <q-layout view="hHh lpR fFf" class="column full-height bg-grey-8 text-grey-1"
+            @dragenter="dragenter"  @keydown.shift.space="scroll_px(0+70-scrollArea.clientHeight)"  @keydown.space.exact="scroll_px(scrollArea.clientHeight-20)"
             style="z-index: 0">
     <q-header reveal height-hint="98" v-model="showHeader">
       <q-toolbar class="bg-grey-9">
@@ -97,17 +119,21 @@ let fitWidth = ref(true);
       </q-toolbar>
     </q-header>
     <q-page-container class="col column">
-      <div class="main col scroll-y smooth text-center" :class="{'fit-width':fitWidth}" v-scroll="scroll"
-           ref="scrollArea">
-        <img v-for="(i,ii) in files" :src="createUrl(i)" :alt="i[1].name" :id="ii.toString()" draggable="false"
-             loading="lazy"/>
+      <div class="col column relative-position">
+        <div class="left-page control" @click="scroll_px(0+70-scrollArea.clientHeight)"><q-icon name="fa-solid fa-circle-left"/></div>
+        <div class="right-page control" @click="scroll_px(scrollArea.clientHeight-20)"><q-icon name="fa-solid fa-circle-right"/></div>
+        <div class="up-page control" @click="scroll_top"><q-icon name="fa-solid fa-circle-up"/></div>
+        <div class="down-page control" @click="scroll_end"><q-icon name="fa-solid fa-circle-down"/></div>
+      <div class="main col scroll-y smooth text-center relative-position" :class="{'fit-width':fitWidth}" v-scroll="scroll" ref="scrollArea">
+        <img v-for="(i,ii) in files" :src="createUrl(i)" :alt="i[1].name" :id="ii.toString()" draggable="false" loading="lazy"/>
         <div v-if="files.size===0" class="text-h5 q-pa-lg">drop folder or image files to this page</div>
       </div>
+      </div>
     </q-page-container>
-    <q-drawer show-if-above v-model="leftDrawerOpen" side="right" bordered>
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="right" width="323" class="text-center" bordered>
       <div class="thumbnail bg-grey-8 smooth full-height scroll-y" ref="scrollArea2">
-        <a v-for="(i,ii) in files" :href="'#'+ii" draggable="false">
-          <img :src="createUrl(i)" :alt="i[1].name" :id="'t-'+ii.toString()" draggable="false" width="280"
+        <a v-for="(i,ii) in files" draggable="false" @click.stop="scrollSync(true)">
+          <img :src="createUrl(i)" :alt="i[1].name" :id="'t-'+ii.toString()" draggable="false" width="300"
                loading="lazy"/>
         </a>
       </div>
@@ -149,7 +175,69 @@ let fitWidth = ref(true);
 
 .thumbnail {
   img {
+    cursor: pointer;
+  }
+}
 
+.control{
+  display: inline-block;
+  position: absolute;
+  z-index: 9;
+  text-align: center;
+  padding: 30px 8%;
+  right: 22px;
+  cursor: pointer;
+  &.up-page{
+    top: 0;
+    left: 0;
+  }
+  &.down-page{
+    bottom: 0;
+    left: 0;
+  }
+  &.left-page{
+    z-index: 10;
+    top: 0;
+    left: 0;
+    width: fit-content;
+    opacity: 0;
+    height: 100%;
+    >i{
+      top: 50%;
+      transform:translateY(-50%);
+    }
+  }
+  &.right-page{
+    z-index: 10;
+    top: 0;
+    height: 100%;
+    opacity: 0;
+    >i{
+      top: 50%;
+      transform:translateY(-50%);
+    }
+  }
+  >i{
+    position: relative;
+    z-index: 1;
+    color: #1abfff;
+    font-size: 70px;
+    opacity: 0;
+    &:after{
+      z-index: -1;
+      content: '';
+      position: absolute;
+      left: 0;
+      right: 0;
+      top: 0;
+      bottom: 0;
+      background-color: #ace1ff;
+      border-radius: 100px;
+      filter: blur(1px);
+    }
+  }
+  &:hover>i{
+    opacity: 0.2;
   }
 }
 </style>
